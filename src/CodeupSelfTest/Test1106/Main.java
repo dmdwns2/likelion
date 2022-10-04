823c172d5f2ab6ca1bc7fea055aea82d27abd62e
김응준 <ssreki1@naver.com>
2022-10-04 23:40:25 +0900

    Int형 범위 구하기 1. 정수형은 4byte로 32bit 임을 16진수로 표현한다 2. 최소값 1000 0000 ... =  0x8000, 최대값 01111 1111 ... = 0x7FFF

---
+++ src/CodeupSelfTest/Test1106/Main.java
@@ -0,0 +1,9 @@
+package CodeupSelfTest.Test1106;
+
+public class Main {
+    public static void main(String[] args) {
+        final int min = 0x80000000;
+        final int max = 0x7FFFFFFF;
+        System.out.println(min+" "+max);
+    }
+}
