79bf69e0fd9f973d6c596e820d109fc7d160bfd8
김응준 <ssreki1@naver.com>
2022-10-04 16:14:53 +0900

    홀수 짝수 구해서 더하기

---
+++ src/EvenOdd2/Main.java
@@ -0,0 +1,27 @@
+package EvenOdd2;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static String isEven(int num){
+        if(num % 2 == 0){
+            return "짝수";
+        }
+        else return "홀수";
+    }
+
+    public static void printEvenOddStatement(int input1, int input2){
+        System.out.println(isEven(input1)+"+"+isEven(input2)+"="+isEven(input1+input2));
+    }
+
+    public static void main(String[] args) {
+        int input1,input2;
+        Scanner sc = new Scanner(System.in);
+        input1 = sc.nextInt();
+        input2 = sc.nextInt();
+        printEvenOddStatement(input1, input2);
+
+
+    }
+}
