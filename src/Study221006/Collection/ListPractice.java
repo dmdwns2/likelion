d013cb5255837e9bdfeee27f0e60ced20464798d
dmdwns2 <ssreki1@naver.com>
2022-10-06 09:43:41 +0900

    컬렉션 복습

---
+++ src/Study221006/Collection/ListPractice.java
@@ -0,0 +1,26 @@
+package Study221006.Collection;
+
+import Study221005.Collection.Names;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListPractice {
+
+    private List<String> students;
+
+
+    public ListPractice(){
+        students = new ArrayList<>();
+        this.students.add("권하준");
+        this.students.add("조성윤");
+        this.students.add("안예은");
+        this.students.add("남우빈");
+        this.students.add("최경민");
+        this.students.add("안준휘");
+    }
+    public List<String> getNames() {
+        return this.students;
+    }
+}
+
