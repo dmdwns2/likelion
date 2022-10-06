038799db34b2d1b115b32cc0c9070cb8b3cc6dc3
dmdwns2 <ssreki1@naver.com>
2022-10-06 13:25:38 +0900

    Map 연습

--- src/Study221006/Map/IsAlphabet.java
+++ src/Study221006/Map/IsAlphabet.java
@@ -3,7 +3,7 @@ package Study221006.Map;
 public class IsAlphabet {
 
     public boolean isAlphabet(char num) {
-        if ((num > 64 && num < 91) || (num > 96 && num < 123)){
+        if ((num >= 'A' && num <= 'Z') || (num >= 'a' && num <= 'z')){
             return true;
         }
         else return false;
