fdf71f231581559974cee610d0ab101ba7b27828
dmdwns2 <ssreki1@naver.com>
2022-10-06 14:22:46 +0900

    File연습 한바이트씩 읽기

---
+++ src/Study221006/File/ReadFileMain.java
@@ -0,0 +1,35 @@
+package Study221006.File;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ReadFileMain {
+
+    private String filename;
+
+    public ReadFileMain(String filename) {
+        this.filename = filename;
+    }
+
+    char readAByte(String filename) {
+        try(BufferedReader br = Files.newBufferedReader(Paths.get("a_file.txt"), StandardCharsets.UTF_8)) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                for (int i = 0; i< line.length(); i++)
+                System.out.println(line.charAt(i));
+            }
+        }catch(IOException e){
+                throw new RuntimeException(e);
+            }
+        return ' ';
+    }
+
+    public static void main(String[] args) {
+        ReadFileMain readFileMain = new ReadFileMain("");
+        readFileMain.readAByte("a_file.txt");
+    }
+}
