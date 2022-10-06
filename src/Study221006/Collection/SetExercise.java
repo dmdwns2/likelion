395f15e5ea9fa0aa582db783ab76c70a45a71402
dmdwns2 <ssreki1@naver.com>
2022-10-06 10:07:31 +0900

    컬렉션

---
+++ src/Study221006/Collection/SetExercise.java
@@ -0,0 +1,11 @@
+package Study221006.Collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetExercise {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>(); // tree set List보다는 Set사용을 추천.
+                                            // 순서도 없고, 중복을 사용하지 않기 때문에 같은거 두개를 넣어도 하나만 나옴
+    }
+}
