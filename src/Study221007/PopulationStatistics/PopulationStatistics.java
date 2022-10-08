94549892dcc2d84c1c43c1b4a8e680e561b55005
dmdwns2 <ssreki1@naver.com>
2022-10-09 05:10:03 +0900

    10.07 인구이동 데이터 받아서 매핑 하고 파일 생성,쓰기한 뒤에 히트맵작성

--- src/Study221007/PopulationStatistics/PopulationStatistics.java
+++ src/Study221007/PopulationStatistics/PopulationStatistics.java
@@ -123,21 +123,23 @@ public class PopulationStatistics {
 
     public Map<String, Integer> heatmapIdxMap(){
         Map<String, Integer> map = new HashMap<>();
-        map.put("11", 0);
-        map.put("26", 1);
-        map.put("27", 2);
-        map.put("28", 3);
-        map.put("29", 4);
-        map.put("30", 5);
-        map.put("41", 6);
-        map.put("42", 7);
-        map.put("43", 8);
-        map.put("44", 9);
-        map.put("45", 10);
-        map.put("46", 11);
-        map.put("47", 12);
-        map.put("48", 13);
-        map.put("50", 14);
+        map.put("11",0);
+        map.put("26",1);
+        map.put("27",2);
+        map.put("28",3);
+        map.put("29",4);
+        map.put("30",5);
+        map.put("31",6);
+        map.put("36",7);
+        map.put("41",8);
+        map.put("42",9);
+        map.put("43",10);
+        map.put("44",11);
+        map.put("45",12);
+        map.put("46",13);
+        map.put("47",14);
+        map.put("48",15);
+        map.put("50",16);
         return map;
     }
 
