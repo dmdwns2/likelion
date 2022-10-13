bd79e0d78517eb9bca8a1f0933beac68df2fe443
dmdwns2 <ssreki1@naver.com>
2022-10-13 09:56:05 +0900

    1013 실습코드 (버블정렬)

--- src/Study221013/bubbleSort01.java
+++ src/Study221013/bubbleSort01.java
@@ -3,17 +3,22 @@ package Study221013;
 class SortAndShow {
     public int[] sort(int[] arr) {
         int index = 0;
-        int temp;
-        for (int i = index + 1; i < arr.length; i++) {
-            if (arr[index] > arr[i]) {
-                temp = arr[index];
-                arr[index] = arr[i];
-                arr[i] = temp;
+        while (true) {
+            for (int i = index + 1; i < arr.length; i++) {
+                if (arr[index] > arr[i]) {
+                    int temp = arr[index];
+                    arr[index] = arr[i];
+                    arr[i] = temp;
+                }
             }
+
+            if (index == arr.length - 1)
+                return arr;
+
             index++;
         }
 
-        return arr;
+
     }
 
     public void showArr(int[] arr) {
@@ -27,7 +32,7 @@ public class bubbleSort01 {
 
     public static void main(String[] args) {
         SortAndShow sortAndShow = new SortAndShow();
-        int[] arr = {7, 2, 3, 9, 28, 11};
+        int[] arr = {7, 2, 3, 9, 28, 11, 10, 20, 30, 40, 50, 2, 31};
 
         sortAndShow.sort(arr);
         sortAndShow.showArr(arr);
