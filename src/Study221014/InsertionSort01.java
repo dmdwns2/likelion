ed1aeae7391685cf8499ba4c069b18ecb530a3ba
dmdwns2 <ssreki1@naver.com>
2022-10-14 09:36:48 +0900

    1014 삽입정렬

---
+++ src/Study221014/InsertionSort01.java
@@ -0,0 +1,19 @@
+package Study221014;
+
+public class InsertionSort01 {
+
+    int key = 1;
+    public int[] sort(int[] arr) {
+
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8, 5, 6, 2, 4};
+
+
+        InsertionSort01 insertionSort01 = new InsertionSort01();
+        int[] result = insertionSort01.sort(arr);
+        System.out.println(result);
+    }
+}
