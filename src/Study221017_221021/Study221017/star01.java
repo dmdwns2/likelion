2c81535128c3051571c31bd98ef2ca023722e5a6
dmdwns2 <ssreki1@naver.com>
2022-10-17 09:10:19 +0900

    221017 알고리즘 실습 별찍기

---
+++ src/Study221017_221021/Study221017/star01.java
@@ -0,0 +1,11 @@
+package Study221017_221021.Study221017;
+
+public class star01 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
+    }
+}
