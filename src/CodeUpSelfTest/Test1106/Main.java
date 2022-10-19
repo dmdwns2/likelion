e28d4bb4d768c4723f5f2856f21fb7bd5447728f
dmdwns2 <ssreki1@naver.com>
2022-10-19 10:23:52 +0900

    Test1106

--- src/CodeUpSelfTest/Test1106/Main.java
+++
@@ -1,9 +0,0 @@
-package CodeUpSelfTest.Test1106;
-
-public class Main {
-    public static void main(String[] args) {
-        final int min = 0x80000000;
-        final int max = 0x7FFFFFFF;
-        System.out.println(min+" "+max);
-    }
-}
