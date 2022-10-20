446a0e1594fdb354826a904dac9f471ef8623429
dmdwns2 <ssreki1@naver.com>
2022-10-20 10:33:49 +0900

    221020 실습 스택2

--- src/Study221017_221021/Study221020/Stack02Test.java
+++ src/Study221017_221021/Study221020/Stack02Test.java
@@ -13,12 +13,12 @@ import static org.junit.jupiter.api.Assertions.*;
 public class Stack02Test {
 
     @BeforeEach
-    void setup(){
+    void setup() {
         System.out.println("before each");
     }
 
     @Test
-    void pushTest(){
+    void pushTest() {
         Stack02 stack02 = new Stack02();
         stack02.push(10);
         stack02.push(20);
@@ -43,13 +43,13 @@ public class Stack02Test {
         stack02.push(30);
         assertEquals(30, stack02.pop());
 
-        assertThrows(EmptyStackException.class,()-> //모던 자바표현
+        assertThrows(EmptyStackException.class, () -> //모던 자바표현
                 stack02.pop()  // 이 구간을 인터페이스라고 생각하면 됨
         );
     }
 
     @Test
-    void isEmpty(){
+    void isEmpty() {
         Stack02 stack02 = new Stack02();
         assertTrue(stack02.isEmpty());
         stack02.push(10);
@@ -57,17 +57,20 @@ public class Stack02Test {
         stack02.pop();
         assertTrue(stack02.isEmpty());
     }
+
     @Test
-    void peek(){
+    void peek() {
         Stack02 stack02 = new Stack02();
-        stack02.peek();
+        assertThrows(EmptyStackException.class, () -> //모던 자바표현
+                stack02.peek()
+        );
     }
 
     @Test
     void realStack() {
         Stack<Integer> st = new Stack<>();
 
-        assertThrows(EmptyStackException.class,()-> //모던 자바표현
+        assertThrows(EmptyStackException.class, () -> //모던 자바표현
                 st.pop()  // 이 구간을 인터페이스라고 생각하면 됨
         );
     }
