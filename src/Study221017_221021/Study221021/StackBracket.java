af39dbd75bc39026fc1295a51fa8bf84192ce550
dmdwns2 <ssreki1@naver.com>
2022-10-21 10:38:51 +0900

    221021 실습 스택사용해서 괄호풀기

--- src/Study221017_221021/Study221021/StackBracket.java
+++ src/Study221017_221021/Study221021/StackBracket.java
@@ -1,12 +1,14 @@
 package Study221017_221021.Study221021;
 
+import java.util.EmptyStackException;
 import java.util.Stack;
 
 public class StackBracket {
 
     public boolean solution(String s){
         Stack<String> stack = new Stack<>();
-
+            if(s.charAt(0) == ')')
+                throw new EmptyStackException();
             for(int i = 0; i < s.length() ; i++) {
                if (s.charAt(i) == '(')
                    stack.push("\\(");
