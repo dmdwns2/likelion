8404e684fe276789044b69631b2745a8f2df3bd0
dmdwns2 <ssreki1@naver.com>
2022-10-25 10:33:25 +0900

    221025 도전 마라톤완주

---
+++ src/Study221024_221028/Study221025/UnfinishedPlayer.java
@@ -0,0 +1,28 @@
+package Study221024_221028.Study221025;
+
+import java.util.Set;
+
+public class UnfinishedPlayer {
+    private int size = 100000;
+
+    private int[] table = new int[size];
+
+    public UnfinishedPlayer(int size) {
+        this.size = size;
+        this.table = new int[size];
+    }
+
+    public UnfinishedPlayer(){
+
+    }
+
+    public static void main(String[] args) {
+        UnfinishedPlayer up = new UnfinishedPlayer();
+        String[] participant = new String[up.size];
+        String[] completion = new String[up.size];
+//          오늘은 여기까찌.
+//        Set<Integer>
+
+    }
+
+}
