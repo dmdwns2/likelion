bb3ddb8661246f209cbe46272124c97e2ff2a1ea
dmdwns2 <ssreki1@naver.com>
2022-10-27 10:38:47 +0900

    221027 도전 마라톤완주

--- src/Study221024_221028/Study221026/UnfinishedPlayer.java
+++ src/Study221024_221028/Study221026/UnfinishedPlayer.java
@@ -10,20 +10,26 @@ import java.util.Map;
 public class UnfinishedPlayer {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> memo = new HashMap<>();
-        for (int i = 0; i < participant.length; i++) {
+        List<Map<String, Integer>>[] table = new ArrayList[participant.length];
+        int result;
+
+        for (int i = 0; i < participant.length; i++) { // 참가자들의 value를 1로 주고
             String key = participant[i];
             memo.put(key, 1);
+            table[i].add(memo);
         }
-
-        for(int i = 0; i < completion.length; i++){
+        for (int i = 0; i < completion.length; i++) {  // 완주한 사람들의 value를 0으로 변경 그러면 한명만 남는다.
             String key = completion[i];
-            memo.put(key,0);
+            memo.put(key, 0);
+            table[i].add(memo);
         }
-        for(String key : memo.keySet()){
-            if (memo.get(key) == 1) {
-                return key;
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                if (table[i].get(j).get(memo.get(1)) == 1)
+                    return "";
             }
         }
+
         return "";
     }
 
