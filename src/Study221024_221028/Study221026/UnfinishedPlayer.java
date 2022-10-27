883df8d7cb21cdf3d90d7a024042ff127914e1b0
dmdwns2 <ssreki1@naver.com>
2022-10-27 09:41:05 +0900

    221027 도전 마라톤완주

--- src/Study221024_221028/Study221026/UnfinishedPlayer.java
+++ src/Study221024_221028/Study221026/UnfinishedPlayer.java
@@ -1,78 +1,33 @@
 package Study221024_221028.Study221026;
 
+import Study221024_221028.Study221025.HashTable;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UnfinishedPlayer {
-
-    class Node {
-        private String key;
-        private Integer value;
-
-        public Node(String key, Integer value) {
-            this.key = key;
-            this.value = value;
-        }
-        public String getKey() {
-            return key;
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> memo = new HashMap<>();
+        for (int i = 0; i < participant.length; i++) {
+            String key = participant[i];
+            memo.put(key, 1);
         }
 
-        public Integer getValue() {
-            return value;
+        for(int i = 0; i < completion.length; i++){
+            String key = completion[i];
+            memo.put(key,0);
         }
-    }
-
-
-    private int size = 1000;
-    private List<Node>[] table = new ArrayList[size];
-
-    public int hash(String key) {
-        int asciiSum = 0;
-        for (int i = 0; i < key.length(); i++) {
-            asciiSum += key.charAt(i);
-        }
-        return asciiSum % size;
-    }
-
-    public void insert(String key, int value) {
-        // List
-        int hashIdx = hash(key);
-        if (this.table[hashIdx] == null) {
-            this.table[hashIdx] = new ArrayList<>();
-        }
-        // Map, Object
-        this.table[hashIdx].add(new Node(key, value));
-    }
-
-
-
-
-/* get 튜닝과정 1
-   public int get(String key){
-        List<Node> nodes = this.table[hash(key)];
-        return this.table[hash(key)];
-    }*/
-
-    //get 튜닝과정 2
-    public Integer get(String key) {
-        List<Node> nodes = this.table[hash(key)];
-        for (Node node : nodes) {
-            if (key.equals(node.getKey())) {
-                return node.value;
+        for(String key : memo.keySet()){
+            if (memo.get(key) == 1) {
+                return key;
             }
         }
-        return null;
+        return "";
     }
 
-
-
     public static void main(String[] args) {
-
-        UnfinishedPlayer up = new UnfinishedPlayer();
-        String[] participant = new String[up.size];
-        String[] completion = new String[up.size-1];
-ㅌ
-
 
     }
 }
