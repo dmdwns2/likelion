67ebefc43752a3465357869583150afd5ef694d8
dmdwns2 <ssreki1@naver.com>
2022-10-28 10:47:06 +0900

    [알고리즘] 전화번호목록 진행중 실행 결과 반반

--- src/Study221024_221028/Study221028/PhoneBook.java
+++ src/Study221024_221028/Study221028/PhoneBook.java
@@ -5,15 +5,12 @@ import java.util.HashSet;
 public class PhoneBook {
     class Solution {
         public boolean solution(String[] phone_book) {
-            boolean answer = true;
             HashSet<String> hs = new HashSet<>();
             for (var num : phone_book) hs.add(num); // hash set에 중복없이 전화번호를 담음
 
             for (int i = 0; i < phone_book.length; i++) {
-                for( int j = 0; j < phone_book.length ; j++) {
                     if(hs.contains(phone_book[j]))
                         return false;
-                }
             }
             return true;
         }
