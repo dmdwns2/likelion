b9bf9d227392ea3eb0446c1aa7fa758b6490ab4b
dmdwns2 <ssreki1@naver.com>
2022-10-28 12:59:54 +0900

    [알고리즘] 폰켓몬 프로그래머스
    조합으로 변경

--- src/Study221024_221028/Study221028/Poketmon.java
+++ src/Study221024_221028/Study221028/Poketmon.java
@@ -8,7 +8,7 @@ import java.util.Set;
 public class Poketmon {
 
     public int solution(int[] nums) {
-        Factorial factorial = new Factorial();
+
         // Set에 포켓몬 배열을 받음
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
@@ -16,21 +16,15 @@ public class Poketmon {
         }
         System.out.println(set.size()); // 중복 분리 확인
 
-
-        if (set.size() % 2 == 0) { // 중복된 배열이 짝수인 경우  2, 4, 6
-            return factorial.factorial(set.size())/2; // 그냥 두개 집는다. nC2
-        } else {
-            return factorial.factorial(set.size())/2+1;
-        }
+        return factorial(set.size()) / 2; // 그냥 두개 집는다. nC2
 
     }
 
-    public class Factorial {
-        public int factorial(int n) {
-            if (n <= 1) return n;
-            else return factorial(n - 1) * n;
-        }
+    public int factorial(int n) {
+        if (n <= 1) return n;
+        else return factorial(n - 1) * n;
     }
+
 
     public static void main(String[] args) {
         Poketmon poketmon = new Poketmon();
