ccbdf1d9968ba019d247f41c007b9efc639c45ed
dmdwns2 <ssreki1@naver.com>
2022-10-28 10:33:03 +0900

    [알고리즘] 221028 포켓몬 프로그래머스 홀수 짝수로 나눠서 팩토리얼 사용

--- src/Study221024_221028/Study221028/Poketmon.java
+++ src/Study221024_221028/Study221028/Poketmon.java
@@ -3,6 +3,8 @@ package Study221024_221028.Study221028;
 import java.util.HashSet;
 import java.util.Set;
 
+
+// 결과 실패
 public class Poketmon {
 
     public int solution(int[] nums) {
@@ -16,9 +18,9 @@ public class Poketmon {
 
 
         if (set.size() % 2 == 0) { // 중복된 배열이 짝수인 경우  2, 4, 6
-            return factorial.factorial(set.size()/2); // 그냥 두개 집는다. n팩토리얼
+            return factorial.factorial(set.size())/2; // 그냥 두개 집는다. nC2
         } else {
-            return factorial.factorial(set.size()/2+1);
+            return factorial.factorial(set.size())/2+1;
         }
 
     }
