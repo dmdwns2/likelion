adc6635cd70bb35a13a2a50522f61a18d3cbcdb2
dmdwns2 <ssreki1@naver.com>
2022-11-01 10:34:39 +0900

    [알고리즘] 실습1 소수 판별하기 콜백 템플릿

--- src/Study221031_221104/Study221101/CallbackPrime.java
+++ src/Study221031_221104/Study221101/CallbackPrime.java
@@ -4,12 +4,15 @@ package Study221031_221104.Study221101;
 실습1
 13, 17, 19, 23 이 소수인지 판별하는 알고리즘을 만들어 보고 소수인지 구해 보세요.
  */
+
+interface StatementStrategy {
+    boolean compare(int i, int num);
+}
+
 public class CallbackPrime {
 
-    interface Callback {
-        boolean compare(int i, int num);
-    }
-    public boolean isDecimal(int num) {
+
+    public boolean isDecimal(int num, StatementStrategy stmt) {
         for (int i = 2; compare(i, num); i++) {
             if (num % i == 0) {
                 System.out.println("소수가 아닙니다.");
