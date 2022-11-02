a5479d6eccf8967a5fe77c7e04522210b9c96b0e
dmdwns2 <ssreki1@naver.com>
2022-11-02 09:47:45 +0900

    [알고리즘] 에라토테네스의 체 알고리즘 미완성

---
+++ src/Study221031_221104/Study221102/PrimeChe.java
@@ -0,0 +1,41 @@
+package Study221031_221104.Study221102;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeChe {
+
+
+
+    private final int SIZE = 50;
+    int index = 2;
+
+    List<Integer> list = new ArrayList<>(SIZE);
+
+
+    public void listSetup() {
+        for ( int i = 2;i <= SIZE; i++) {
+            list.add(i);
+        }
+    }
+
+    public void che() {
+        while(index <= SIZE) {
+            if (list.contains(index)) {
+                for (int i = index * index; i <= SIZE; ) {
+                    list.remove(Integer.valueOf(i));
+                    i *= index;
+                }
+            }
+            System.out.println(list);
+            index++;
+        }
+    }
+
+    public static void main(String[] args) {
+        PrimeChe primeChe = new PrimeChe();
+        primeChe.listSetup();
+        primeChe.che();
+    }
+
+}
