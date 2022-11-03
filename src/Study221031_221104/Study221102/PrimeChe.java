27e62bcf38f0cd38fb0d09e70614d9b1a73374ce
dmdwns2 <ssreki1@naver.com>
2022-11-03 09:23:27 +0900

    [알고리즘] 에라토테네스의 체 알고리즘 시간복잡도 해결 완성
    List제거

--- src/Study221031_221104/Study221102/PrimeChe.java
+++ src/Study221031_221104/Study221102/PrimeChe.java
@@ -10,11 +10,11 @@ public class PrimeChe {
     private final int SIZE = 2535;
     int index = 2;
 
-    List<Integer> list = new ArrayList<>(SIZE);
+//    List<Integer> list = new ArrayList<>(SIZE);
     boolean[] arr = new boolean[SIZE-1];
     public void listSetup() {
         for ( int i = 2;i <= SIZE; i++) {
-            list.add(i);
+//            list.add(i);
             arr[i-2] = true;
         }
 
@@ -22,14 +22,14 @@ public class PrimeChe {
 
     public void che() {
         while(index <= SIZE) {
-            if (list.contains(index)) {
+//            if (list.contains(index)) {
                 for (int i = index * index; i <= SIZE; ) {
                     arr[i-2] = false;
 //                    list.remove(Integer.valueOf(i)); 이렇게하면 시간복잡도 문제 발생
                     i += index;
 //                    System.out.println("i "+i+" index "+index);
                 }
-            }
+//            }
             index++;
         }
     }
@@ -38,7 +38,7 @@ public class PrimeChe {
         for(int i = 0; i < SIZE-1 ; i++){
             if(arr[i] == true)
                 cnt++;
-//            System.out.println(arr[i]); 확인용
+            System.out.println(arr[i]); //확인용
         }
 
         System.out.println(cnt);
