ec8d3f1ade5403c0154a520241d2d77419b69261
dmdwns2 <ssreki1@naver.com>
2022-11-03 09:49:29 +0900

    [알고리즘] 에라토테네스의 체 알고리즘
    index * index 때문에
    
    제곱해서 int범위를 벗어나면 안되는 문제를
    
    index * 2로 변경해서 줄임

--- src/Study221031_221104/Study221102/PrimeChe.java
+++ src/Study221031_221104/Study221102/PrimeChe.java
@@ -10,11 +10,9 @@ public class PrimeChe {
     private final int SIZE = 2535;
     int index = 2;
 
-//    List<Integer> list = new ArrayList<>(SIZE);
     boolean[] arr = new boolean[SIZE-1];
     public void listSetup() {
         for ( int i = 2;i <= SIZE; i++) {
-//            list.add(i);
             arr[i-2] = true;
         }
 
@@ -22,12 +20,9 @@ public class PrimeChe {
 
     public void che() {
         while(index <= SIZE) {
-//            if (list.contains(index)) {
-                for (int i = index * index; i <= SIZE; ) {
+                for (int i = index * 2; i <= SIZE; ) {
                     arr[i-2] = false;
-//                    list.remove(Integer.valueOf(i)); 이렇게하면 시간복잡도 문제 발생
                     i += index;
-//                    System.out.println("i "+i+" index "+index);
                 }
 //            }
             index++;
