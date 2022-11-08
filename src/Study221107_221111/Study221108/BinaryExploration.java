cb75da8dfd2d147a203aa31cd2dc9f9587483abc
dmdwns2 <ssreki1@naver.com>
2022-11-08 10:23:48 +0900

    [알고리즘] 이진 탐색 _프로그래머 스 미완성

---
+++ src/Study221107_221111/Study221108/BinaryExploration.java
@@ -0,0 +1,29 @@
+package Study221107_221111.Study221108;
+
+public class BinaryExploration {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+        final int TARGET = 10;
+        int indexStart = 0;
+        int indexEnd = nums.length;
+        int median = nums.length / 2;
+
+        // 중간값 찾기
+        // 인덱스(시작점, 끝점) 옮기기
+        // 같은지 비교하기
+        while (true) {
+            for (int i = 0; i < indexEnd; i++)
+                if (nums[median] == TARGET)
+                    System.out.println("찾는 값은" + (median + 1) + " 번 째에 있습니다. 값 : " + nums[median]);
+                else if (nums[median] > TARGET) {
+                    indexEnd = median;
+                    median = (indexEnd - indexStart) / 2;
+                    break;
+                } else {
+                    indexStart = median;
+                    median = (indexEnd - indexStart) / 2;
+                }
+        }
+    }
+}
