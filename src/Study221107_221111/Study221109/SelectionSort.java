a61de92b110d0b3183e0280c4d25c146cfcac3b4
dmdwns2 <ssreki1@naver.com>
2022-11-09 09:50:07 +0900

    [알고리즘] 선택정렬 _프로그래머 스 미완성
    미완성입니다.

---
+++ src/Study221107_221111/Study221109/SelectionSort.java
@@ -0,0 +1,32 @@
+package Study221107_221111.Study221109;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+        int index = 0;
+        int temp;
+        int min = arr[0];
+        int iVal = 0;
+        boolean isChange = false;
+
+        while (index < arr.length) {
+            for (int i = index; i < arr.length; i++) {
+                if (min > arr[i]) {
+                    min = arr[i];
+                    isChange = true;
+                    iVal = i;
+                }
+            }
+            if(isChange == true) {
+                temp = arr[index];
+                arr[index] = min;
+                arr[iVal] = temp;
+            }
+            isChange = false;
+            index++;
+        }
+
+        for(int i = 0; i < arr.length; i++)
+        System.out.println(arr[i]);
+    }
+}
