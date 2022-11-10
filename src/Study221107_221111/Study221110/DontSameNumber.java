0b248b4ed9a4c1aaf816c636c36a48ca8dc9c62a
dmdwns2 <ssreki1@naver.com>
2022-11-10 09:17:51 +0900

    [알고리즘] 같은 숫자는 싫어 _프로그래머스 answer의 개수에서 걸림

--- src/Study221107_221111/Study221110/DontSameNumber.java
+++ src/Study221107_221111/Study221110/DontSameNumber.java
@@ -2,7 +2,12 @@ package Study221107_221111.Study221110;
 
 public class DontSameNumber {
     public int[] solution(int[] arr) {
-        int[] answer = {};
+        int SIZE = 0;
+        for(int i = 1; i <arr.length; i++) {
+            if (arr[i] == arr[i-1]);
+            else SIZE += 1;
+        }
+        int[] answer = new int[SIZE];
         int index = 1;
         for (int i = 1; i < arr.length; i++){
             answer[0] = arr[0];
