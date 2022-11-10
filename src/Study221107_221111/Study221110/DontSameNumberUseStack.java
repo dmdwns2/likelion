36f7056bbe7f0b1d5b71751988169862a1a27275
dmdwns2 <ssreki1@naver.com>
2022-11-10 09:32:24 +0900

    [알고리즘] 같은 숫자는 싫어_스택사용 _프로그래머스 완성

---
+++ src/Study221107_221111/Study221110/DontSameNumberUseStack.java
@@ -0,0 +1,19 @@
+package Study221107_221111.Study221110;
+
+import java.util.Stack;
+
+public class DontSameNumberUseStack {
+    public Object[] solution(int[] arr) {
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] == arr[i - 1]) ;
+            else stack.push(arr[i]);
+        }
+
+        Object[] answer = stack.toArray();
+        return answer;
+    }
+}
