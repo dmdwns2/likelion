92f3faea469359b93cb3f0e81f7263b3f615f9d7
dmdwns2 <ssreki1@naver.com>
2022-11-11 09:30:27 +0900

    [알고리즘] 비밀지도 _프로그래머스  미완성

--- src/Study221107_221111/Study221111/SecretMap.java
+++ src/Study221107_221111/Study221111/SecretMap.java
@@ -1,16 +1,20 @@
 package Study221107_221111.Study221111;
 
 public class SecretMap {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
+        public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[arr1.length];
         String[] binary1 = new String[arr1.length];
         String[] binary2 = new String[arr2.length];
-        for(int i = 0; i < arr1.length; i++) {
-            binary1[i] = Integer.toBinaryString(arr1[i]);
-        }for(int i = 0; i < arr2.length; i++) {
-            binary2[i] = Integer.toBinaryString(arr1[i]);
+
+
+        for(int i = 0; i< arr1.length; i++){
+            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+            System.out.println(answer[i]);
         }
 
         return answer;
+    }
+    public static void main(String[] args) {
+//        System.out.println(30 | 9); // 확인용
     }
 }
