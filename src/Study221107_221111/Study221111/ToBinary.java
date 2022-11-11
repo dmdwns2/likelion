494573b2ec9d4f1ded9f2c044d450ace236e89f6
dmdwns2 <ssreki1@naver.com>
2022-11-11 10:48:51 +0900

    [알고리즘] 이진 변환 반복하기 _프로그래머스

---
+++ src/Study221107_221111/Study221111/ToBinary.java
@@ -0,0 +1,21 @@
+package Study221107_221111.Study221111;
+
+// 몇번 실행 했고 0이 몇번 지워졌냐
+public class ToBinary {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+        int numOfZero = 0;
+        int cnt = 0;
+//        while (s.contains("0")) {
+        while(true){
+            cnt++;
+            numOfZero += s.length() - s.replace("0", "").length();
+            s = Integer.toBinaryString(s.replace("0", "").length());
+            if(Integer.parseInt(s) == 1) break;
+        }
+
+        answer[0] = cnt;
+        answer[1] = numOfZero;
+        return answer;
+    }
+}
