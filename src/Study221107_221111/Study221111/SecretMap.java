00e86e9c41926be11964b3cbc6151f04628cf12b
dmdwns2 <ssreki1@naver.com>
2022-11-11 10:09:12 +0900

    [알고리즘] 비밀지도 _프로그래머스  완성

--- src/Study221107_221111/Study221111/SecretMap.java
+++ src/Study221107_221111/Study221111/SecretMap.java
@@ -5,7 +5,7 @@ public class SecretMap {
         String[] answer = new String[n];
 
         for (int i = 0; i < n; i++) {
-            answer[i] = String.format("%0"+(n+1)+"d", Integer.parseInt(Integer.toBinaryString(arr1[i] | arr2[i])));
+            answer[i] = String.format("%0"+n+"d", Integer.parseInt(Integer.toBinaryString(arr1[i] | arr2[i])));
             answer[i] = answer[i].replace("1", "#");
             answer[i] = answer[i].replace("0", " ");
             System.out.println(answer[i]);
@@ -17,7 +17,7 @@ public class SecretMap {
 //        System.out.println(30 | 9); // 확인용
         SecretMap sm = new SecretMap();
 //        sm.solution(5, new int[]{9, 20, 28, 18, 11}, new int[]{30, 1, 21, 17, 28});
-        sm.solution(5, new int[]{46, 33, 33, 22, 31, 50}, new int[]{27, 56, 19, 14, 14, 10});
+        sm.solution(6, new int[]{46, 33, 33, 22, 31, 50}, new int[]{27, 56, 19, 14, 14, 10});
 
     }
 }
