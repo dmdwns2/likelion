65617d404620e5b4bc6a2b5851e701343500f21e
dmdwns2 <ssreki1@naver.com>
2022-11-11 09:56:29 +0900

    [알고리즘] 비밀지도 _프로그래머스  완성

--- src/Study221107_221111/Study221111/SecretMap.java
+++ src/Study221107_221111/Study221111/SecretMap.java
@@ -1,23 +1,23 @@
 package Study221107_221111.Study221111;
 
 public class SecretMap {
-        public String[] solution(int n, int[] arr1, int[] arr2) {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-        String[] binary1 = new String[n];
-        String[] binary2 = new String[n];
 
-
-        for(int i = 0; i< n; i++){
-            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
-            answer[i] = answer[i].replace("1","#");
-            answer[i] = answer[i].replace("0"," ");
+        for (int i = 0; i < n; i++) {
+            answer[i] = String.format("%05d", Integer.parseInt(Integer.toBinaryString(arr1[i] | arr2[i]).toString()));
+            answer[i] = answer[i].replace("1", "#");
+            answer[i] = answer[i].replace("0", " ");
             System.out.println(answer[i]);
         }
         return answer;
     }
+
     public static void main(String[] args) {
 //        System.out.println(30 | 9); // 확인용
         SecretMap sm = new SecretMap();
-        sm.solution(5, new int[]{9, 20, 28, 18, 11}, new int[]{30, 1, 21, 17, 28});
+//        sm.solution(5, new int[]{9, 20, 28, 18, 11}, new int[]{30, 1, 21, 17, 28});
+        sm.solution(5, new int[]{46, 33, 33, 22, 31, 50}, new int[]{27, 56, 19, 14, 14, 10});
+
     }
 }
