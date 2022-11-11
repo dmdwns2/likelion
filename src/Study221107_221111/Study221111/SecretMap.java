1182d01f16a5be90b9e365aebe378de8f9833206
dmdwns2 <ssreki1@naver.com>
2022-11-11 09:39:41 +0900

    [알고리즘] 비밀지도 _프로그래머스  완성

--- src/Study221107_221111/Study221111/SecretMap.java
+++ src/Study221107_221111/Study221111/SecretMap.java
@@ -9,7 +9,9 @@ public class SecretMap {
 
         for(int i = 0; i< n; i++){
             answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
-            System.out.println(answer[i].replace("1","#"));
+            answer[i] = answer[i].replace("1","#");
+            answer[i] = answer[i].replace("0"," ");
+            System.out.println(answer[i]);
         }
         return answer;
     }
