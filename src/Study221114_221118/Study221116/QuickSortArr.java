4d3c376aec19b7a902a0b3aaa1cea430a74a85ad
dmdwns2 <ssreki1@naver.com>
2022-11-16 10:28:29 +0900

    [알고리즘] 퀵 정렬 배열사용 _ 미완성

--- src/Study221114_221118/Study221116/QuickSortArr.java
+++ src/Study221114_221118/Study221116/QuickSortArr.java
@@ -1,14 +1,96 @@
 package Study221114_221118.Study221116;
 
+import java.util.Arrays;
+
 public class QuickSortArr {
 
-    public void solution(int[] arr){
+    int cnt = 0;
 
+    public void solution(int[] arr) {
+        sort(arr);
 
     }
 
-    public int[] sort(int[] arr){
-        int pivot = arr[arr.length/2];
+    public int[] merge(int[] left, int[] mid, int[] right) {
+        int[] answer = new int[left.length];
+        int leftIdx = 0;
+        int midIdx = 0;
+        int rightIdx = 0;
+        int forIdx = 0;
+//        for (int i = 0; i < left.length; i++) {
+//            System.out.println("leftIdx : " + leftIdx);
+//            if (left[i] == 0) {
+//                if (i == 0) {
+//                    leftIdx = 0;
+//                    break;
+//                }
+//                leftIdx = i - 1;
+//                break;
+//            }
+//            leftIdx++;
+//        }
+//        for (int i = 0; i < left.length; i++) {
+//            System.out.println("rightIdx : " + rightIdx);
+//            if (right[i] == 0) {
+//                if (i == 0) {
+//                    rightIdx = 0;
+//                    break;
+//                }
+//                rightIdx = i - 1;
+//                break;
+//            }
+//            rightIdx = i;
+//        }
+//        for (int i = 0; i < left.length; i++) {
+//            System.out.println("midIdx : " + midIdx);
+//            if (mid[i] == 0) {
+//                if (i == 0) {
+//                    midIdx = 0;
+//                    break;
+//                }
+//                midIdx = i - 1;
+//                break;
+//            }
+//        }
+
+        //for
+        for (int i = 0; i < answer.length; i++) {
+            if (left[i] == 0) {
+                forIdx = i;
+                System.out.println("for idx : " + forIdx);
+                break;
+            }
+            answer[i] = left[leftIdx];
+            leftIdx++;
+            System.out.println("for answer : " + answer[i]);
+        }
+        for (int i = forIdx; i < answer.length; i++) {
+            if (mid[i] == 0){
+                forIdx = i;
+                System.out.println("for idx : " + forIdx);
+                break;
+            }
+            answer[i] = mid[midIdx];
+            midIdx++;
+            System.out.println("for answer : ");
+        }
+        for (int i = forIdx; i < answer.length; i++) {
+            if (right[i] == 0){
+                System.out.println("for idx : " + forIdx);
+                break;
+            }
+            answer[i] = right[rightIdx];
+            rightIdx++;
+            System.out.println("for answer : ");
+        }
+        System.out.println(answer);
+        return answer;
+    }
+
+    public int[] sort(int[] arr) {
+        cnt++; // 회차
+        System.out.println("현재 : " + cnt);
+        int pivot = arr[arr.length / 2];
         int[] left = new int[arr.length];
         int[] right = new int[arr.length];
         int[] mid = new int[arr.length];
@@ -17,23 +99,32 @@ public class QuickSortArr {
         int midIdx = 0;
         // O O O O /O/ O O O
 
-        for(int i = 0; i < arr.length ; i++){
-            if(pivot > arr[i]){
+        for (int i = 0; i < arr.length; i++) {
+            if (pivot > arr[i]) {
                 left[leftIdx] = arr[i];
                 leftIdx++;
             } else if (pivot < arr[i]) {
                 right[rightIdx] = arr[i];
                 rightIdx++;
-            }else {
+            } else {
                 mid[midIdx] = arr[i];
                 midIdx++;
             }
 
         }
-        for(int i = 0; i< arr.length;i++){
-        System.out.println(left[i] +mid[i] + right[i]);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(left[i] + " ");
         }
-        return arr;
+        System.out.println();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(mid[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(right[i] + " ");
+        }
+        System.out.println();
+        return merge(left, mid, right);
     }
 
     public static void main(String[] args) {
