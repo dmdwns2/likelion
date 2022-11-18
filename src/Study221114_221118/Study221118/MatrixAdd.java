4701076cc9d46e9e034f72db1a4a3ebf6f8c5f57
dmdwns2 <ssreki1@naver.com>
2022-11-18 10:05:40 +0900

    [알고리즘] 행렬의 덧셈 암호 _프로그래머스 완성

--- src/Study221114_221118/Study221118/MatrixAdd.java
+++ src/Study221114_221118/Study221118/MatrixAdd.java
@@ -3,10 +3,10 @@ package Study221114_221118.Study221118;
 public class MatrixAdd {
     static class Solution {
         public int[][] solution(int[][] arr1, int[][] arr2) {
-            int[][] answer = {};
+            int[][] answer = new int[arr1.length][arr1[0].length];
 
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < 2; j++) {
+            for (int i = 0; i < arr1.length; i++) {
+                for (int j = 0; j < arr1[0].length; j++) {
                     answer[i][j] = arr1[i][j] + arr2[i][j];
                 }
             }
