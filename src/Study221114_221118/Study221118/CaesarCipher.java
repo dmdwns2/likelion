5c95b7f0fd0ec2ac870e70b8870a5d7a2363eca5
dmdwns2 <ssreki1@naver.com>
2022-11-18 09:38:26 +0900

    [알고리즘] 시저 암호 _프로그래머스  완성

--- src/Study221114_221118/Study221118/CaesarCipher.java
+++ src/Study221114_221118/Study221118/CaesarCipher.java
@@ -1,14 +1,36 @@
 package Study221114_221118.Study221118;
 
 public class CaesarCipher {
-    static class Solution{
+    static class Solution {
         public String solution(String s, int n) {
             String answer = "";
-            for(int i =0; i < s.length(); i++){
-                if(s.charAt(i) != ' '){
-                    answer += s.charAt(i) + n;
+            char temp1;
+
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == ' ') {
+                    answer += s.charAt(i);
                 }
-                else answer += ' ';
+
+                //소문자
+                if (s.charAt(i) <= 'z' && 'a' <= s.charAt(i)) {
+                    temp1 = s.charAt(i);
+                    for (int j = 1; j <= n; j++) {
+                        temp1++;
+                        if (temp1 > 'z') temp1 = 'a';
+
+                    }
+                    answer += temp1;
+                }
+                // 대문자 같은로직
+                if (s.charAt(i) <= 'Z' && 'A' <= s.charAt(i)) {
+                    temp1 = s.charAt(i);
+                    for (int j = 1; j <= n; j++) {
+                        temp1++;
+                        if (temp1 > 'Z') temp1 = 'A';
+
+                    }
+                    answer += temp1;
+                }
             }
 
             return answer;
@@ -16,7 +38,7 @@ public class CaesarCipher {
     }
 
     public static void main(String[] args) {
-       Solution sol = new Solution();
-       sol.solution("ab",1);
+        Solution sol = new Solution();
+        sol.solution("ab", 1);
     }
 }
