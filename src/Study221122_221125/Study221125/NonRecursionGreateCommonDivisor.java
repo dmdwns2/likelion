e7fd9c1baa1e08dc1cdbc03ba694589cb1c6f8f8
dmdwns2 <ssreki1@naver.com>
2022-11-25 09:43:01 +0900

    [알고리즘] 재귀없이 최대공약수 구하기 완성

---
+++ src/Study221122_221125/Study221125/NonRecursionGreateCommonDivisor.java
@@ -0,0 +1,40 @@
+package Study221122_221125.Study221125;
+
+import java.io.*;
+
+public class NonRecursionGreateCommonDivisor {
+
+    public static int gcd(int num1, int num2){
+        while(num1 != num2) {
+
+            if (num1 > num2){
+                System.out.println(num1 + " " + num2); //확인용
+                num1 = num1-num2;
+                num2 = num2;
+            }
+            else if (num1 < num2) {
+                System.out.println(num1 + " " + num2); //확인용
+                num1 = num1;
+                num2 = num2 - num1;
+            }
+        }
+        return num1;
+    }
+    public static void main(String[] args) throws IOException {
+        // Input
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String str = bf.readLine();
+        String[] strArr = str.split(" ");
+
+        int num1 = Integer.parseInt(strArr[0]);
+        int num2 = Integer.parseInt(strArr[1]);
+        bf.close();
+
+        // Output
+        // Output
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String outStr = String.valueOf(gcd(num1, num2));
+        bw.write(outStr);
+        bw.close();
+    }
+}
