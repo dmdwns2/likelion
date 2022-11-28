e04be5759f83b6019df060511ffbaf574cfd6eef
dmdwns2 <ssreki1@naver.com>
2022-11-28 09:21:35 +0900

    [알고리즘] 알파벳 출력하기 실습1번 완성

---
+++ src/Study221128_221202/Study221128/PrintAlphabet.java
@@ -0,0 +1,14 @@
+package Study221128_221202.Study221128;
+
+public class PrintAlphabet {
+
+    public static void print(){
+
+        for(char ch = 0x41; ch <= 0x5A; ch++)
+        System.out.println(ch);
+    }
+
+    public static void main(String[] args) {
+        print();
+    }
+}
