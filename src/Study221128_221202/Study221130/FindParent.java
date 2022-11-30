6a0e1c789b2d969071d982c8c9c73b8d216bb444
dmdwns2 <ssreki1@naver.com>
2022-11-30 10:01:59 +0900

    [알고리즘] 1936 : (재귀함수) 두 노드간의 거리 미완성 초안

---
+++ src/Study221128_221202/Study221130/FindParent.java
@@ -0,0 +1,17 @@
+package Study221128_221202.Study221130;
+
+
+// 부모 찾기
+
+public class FindParent {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+
+
+        int parentA = Math.floorDiv(3, 2);
+
+        System.out.printf("parentA: %d", parentA);
+    }
+
+}
