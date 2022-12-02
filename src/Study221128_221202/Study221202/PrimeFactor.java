fed8941f3d20225f3a6ca055f8559d00bfe10c34
dmdwns2 <ssreki1@naver.com>
2022-12-02 10:30:18 +0900

    [알고리즘] 코드업 2203 : 소인수구하기 시간초과 리팩토링

--- src/Study221128_221202/Study221202/PrimeFactor.java
+++ src/Study221128_221202/Study221202/PrimeFactor.java
@@ -6,29 +6,34 @@ import java.io.InputStreamReader;
 
 public class PrimeFactor {
 
-    public static void pf(long num){
+    public static void pf(long num) {
 
         // 한번 거르는 과정 소인수 분해
         String result = "";
         int i = 2;
         long temp = num;
-        while(num != i){
-            if(temp % i == 0){ // 나눠지면
+        while (num != i) {
+            if (temp % i == 0) { // 나눠지면
                 temp = temp / i;
                 result += String.format(i + " ");
-            }
-            else {  // 안나눠지면
+            } else {  // 안나눠지면
                 i++;
             }
         }
         //빠져나오면 num = i
-        if(result == ""){ // 소수라면
+        if (result == "") { // 소수라면
             System.out.println(num);
+        } else {  //num이 소수가 아니라면
+            result = result.trim();
+            System.out.println(splitLastString(result,' '));
         }
-        else{  //num이 소수가 아니라면
-            String[] strArr = result.split(" ");
-            System.out.println(strArr[strArr.length-1]);
-        }
+    }
+
+    // 마지막 문자열 가져오기 이것도 시간초과
+    private static String splitLastString(String target, char deliminator) {
+        int x = target.lastIndexOf(deliminator);
+        if (x > 0) return target.substring(x + 1);
+        return target;
     }
 
     public static void main(String[] args) throws IOException {
